607824e66f111edf8939eafca3225b45c4079133
Igor Gregori <igsilvacarlos@gmail.com>
2026-04-17 14:07:54 -0300

    fix bars rendering

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
 const board = document.getElementById("board");
+const maxCanvasWidth = board.offsetWidth;
+const maxCanvasHeight = board.offsetHeight;
 
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-ctx.canvas.width = board.offsetWidth;
-ctx.canvas.height = board.offsetHeight;
+ctx.canvas.width = maxCanvasWidth;
+ctx.canvas.height = maxCanvasHeight;
 
 let arr = [];
+let numberOfBars = 50;
+let barGap = 2;
+let barWidth = maxCanvasWidth / numberOfBars - barGap;
 
-for (let i = 0; i < 100; i++) {
-  arr.push(Math.floor(Math.random() * 500));
+for (let i = 0; i < numberOfBars; i++) {
+  arr.push(Math.floor(Math.random() * maxCanvasHeight * 0.75));
 }
 
-const barWidth = 10;
-let barPos = 10;
 function draw() {
-  // ctx.fillStyle = "white";
-  // ctx.fillRect(10, 10, 10, 10);
+  let barPos = barGap;
   for (let i = 0; i < arr.length; i++) {
     ctx.fillStyle = "#3b3b3b";
     ctx.fillRect(barPos, canvas.height - arr[i], barWidth, arr[i]);
-    barPos += barWidth + 2;
+    barPos += barWidth + barGap;
   }
 }
 
