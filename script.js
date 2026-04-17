8fba4c122adcf1c34623d21e12ce05251ec75d51
Igor Gregori <igsilvacarlos@gmail.com>
2026-04-17 14:34:19 -0300

    add rand feature

--- script.js
+++ script.js
@@ -1,23 +1,39 @@
 const board = document.getElementById("board");
-const maxCanvasWidth = board.offsetWidth;
-const maxCanvasHeight = board.offsetHeight;
+
+const randBtn = document.getElementById("rand-btn");
+const playBtn = document.getElementById("play-btn");
 
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-ctx.canvas.width = maxCanvasWidth;
-ctx.canvas.height = maxCanvasHeight;
+const canvasWidth = board.offsetWidth;
+const canvasHeight = board.offsetHeight;
+ctx.canvas.width = canvasWidth;
+ctx.canvas.height = canvasHeight;
+
+randBtn.addEventListener("click", () => {
+  randomizeArr();
+  draw();
+});
+
+playBtn.addEventListener("click", () => {
+  alert("play");
+});
 
 let arr = [];
 let numberOfBars = 50;
 let barGap = 2;
-let barWidth = maxCanvasWidth / numberOfBars - barGap;
+let barWidth = canvasWidth / numberOfBars - barGap;
 
-for (let i = 0; i < numberOfBars; i++) {
-  arr.push(Math.floor(Math.random() * maxCanvasHeight * 0.75));
+function randomizeArr() {
+  arr = [];
+  for (let i = 0; i < numberOfBars; i++) {
+    arr.push(Math.floor(Math.random() * canvasHeight * 0.75));
+  }
 }
 
 function draw() {
+  ctx.clearRect(0, 0, canvasWidth, canvasHeight);
   let barPos = barGap;
   for (let i = 0; i < arr.length; i++) {
     ctx.fillStyle = "#3b3b3b";
@@ -26,10 +42,5 @@ function draw() {
   }
 }
 
+randomizeArr();
 draw();
-
-// ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-// const intervalID = setInterval(() => {
-//   draw();
-// }, renderTime);
